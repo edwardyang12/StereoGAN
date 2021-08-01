daf65e2077f8dfb53ab36ebd44fd8df3df57d0df
edwardyang12 <edwardyang016@gmail.com>
2021-07-31 21:49:46 -0700

    cleaner code

--- nets/generator.py
+++ nets/generator.py
@@ -1,12 +1,25 @@
 import torch.nn as nn
 
+# going down
 class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.single_conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 4, 2, 1, bias=False),
             nn.BatchNorm2d(out_channels),
-            nn.ReLU(0.2, inplace=True),
+            nn.ReLU(True),
+        )
+    def forward(self, x):
+        return self.single_conv(x)
+
+# going up
+class ConvTransposeBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.single_conv = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(True),
         )
     def forward(self, x):
         return self.single_conv(x)
@@ -15,35 +28,34 @@ class Generator(nn.Module):
     def __init__(self, channels=1, lat_vector=100, feat_map=64):
         super(Generator, self).__init__()
 
-        self.main = nn.Sequential(
-            # input is Z, going into a convolution
-            ConvBlock(channels,feat_map),
-            ConvBlock(feat_map,feat_map*2),
-            ConvBlock(feat_map*2,feat_map*4),
-            ConvBlock(feat_map*4,feat_map*8),
-            ConvBlock(feat_map*8,feat_map*16),
+        # input is Z, going into a convolution
+        self.down1 = ConvBlock(channels,feat_map)
+        self.down2 = ConvBlock(feat_map,feat_map*2)
+        self.down3 = ConvBlock(feat_map*2,feat_map*4)
+        self.down4 = ConvBlock(feat_map*4,feat_map*8)
+        self.down5 = ConvBlock(feat_map*8,feat_map*16)
 
-            nn.ConvTranspose2d( feat_map * 16, feat_map * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(feat_map * 8),
-            nn.ReLU(True),
-            # state size. (feat_map) x 32 x 32
+        self.up1 = ConvTransposeBlock(feat_map*16, feat_map*8)
+        self.up2 = ConvTransposeBlock(feat_map*8, feat_map*4)
+        self.up3 = ConvTransposeBlock(feat_map*4, feat_map*2)
+        self.up4 = ConvTransposeBlock(feat_map*2, feat_map)
 
-            nn.ConvTranspose2d( feat_map * 8, feat_map * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(feat_map * 4),
-            nn.ReLU(True),
+        self.up5 = nn.ConvTranspose2d(feat_map, channels, 4, 2, 1, bias=False)
+        self.out = nn.Tanh()
+        # state size. (channels) x 64 x 64
 
-            nn.ConvTranspose2d( feat_map * 4, feat_map * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(feat_map * 2),
-            nn.ReLU(True),
-
-            nn.ConvTranspose2d( feat_map * 2, feat_map, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(feat_map),
-            nn.ReLU(True),
-
-            nn.ConvTranspose2d( feat_map, channels, 4, 2, 1, bias=False),
-            nn.Tanh()
-            # state size. (channels) x 64 x 64
-        )
 
     def forward(self, input):
-        return self.main(input)
+        x = self.down1(input)
+        x = self.down2(x)
+        x = self.down3(x)
+        x = self.down4(x)
+        x = self.down5(x)
+        x = self.up1(x)
+        x = self.up2(x)
+        x = self.up3(x)
+        x = self.up4(x)
+        x = self.up5(x)
+        x = self.out(x)
+
+        return x
