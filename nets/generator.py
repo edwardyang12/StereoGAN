6fd47ad26d2a899ff499fa5d96a01dd9ea34de5e
VictorTao1998 <jit124@ucsd.edu>
2021-08-02 05:44:06 -0700

    a

--- nets/generator.py
+++ nets/generator.py
@@ -7,17 +7,26 @@ class Generator(nn.Module):
         self.main = nn.Sequential(
             # input is Z, going into a convolution
             nn.Conv2d(channels, feat_map, 3, 1, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.BatchNorm2d(feat_map),
+            nn.ReLU(True),
+            
             nn.Conv2d(feat_map, feat_map * 2, 3, 1, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.BatchNorm2d(feat_map * 2),
+            nn.ReLU(True),
+            
             nn.Conv2d(feat_map * 2, feat_map * 4, 3, 1, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.BatchNorm2d(feat_map * 4),
+            nn.ReLU(True),
+            
             nn.Conv2d(feat_map * 4, feat_map * 8, 3, 1, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.BatchNorm2d(feat_map * 8),
+            nn.ReLU(True),
+            
             # state size. (feat_map) x 32 x 32
             nn.Conv2d(feat_map * 8, feat_map * 16, 3, 1, 1, bias=False),
             nn.BatchNorm2d(feat_map * 16),
-            nn.LeakyReLU(0.2, inplace=True),
+            nn.ReLU(True),
+
             nn.Conv2d(feat_map * 16, 1, 3, 1, 1, bias=False),
             nn.Tanh()
             # state size. (channels) x 64 x 64
