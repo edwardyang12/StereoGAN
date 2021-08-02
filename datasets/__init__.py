bc999668833c4e3c351525fcba3fbe52966e5882
VictorTao1998 <jit124@ucsd.edu>
2021-08-01 18:03:38 -0700

    a

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,7 +3,5 @@ from .custom_test import CustomDatasetTest
 from .messy_dataset import MESSYDataset
 
 __datasets__ = {
-    "custom_dataset_full": CustomDataset,
-    "custom_dataset_test": CustomDatasetTest,
     "messy_table": MESSYDataset
 }
