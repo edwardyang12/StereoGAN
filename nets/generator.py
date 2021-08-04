cccd7b9e87847c8299f949dc84931f7a3da6de28
VictorTao1998 <jit124@ucsd.edu>
2021-08-03 19:49:06 -0700

    a

--- nets/generator.py
+++ nets/generator.py
@@ -23,11 +23,19 @@ class Generator(nn.Module):
             nn.ReLU(True),
             
             # state size. (feat_map) x 32 x 32
-            nn.Conv2d(feat_map * 8, feat_map * 16, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(feat_map * 16),
+            nn.Conv2d(feat_map * 8, feat_map * 4, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(feat_map * 4),
             nn.ReLU(True),
 
-            nn.Conv2d(feat_map * 16, 1, 3, 1, 1, bias=False),
+            nn.Conv2d(feat_map * 4, feat_map * 2, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(feat_map * 2),
+            nn.ReLU(True),
+
+            nn.Conv2d(feat_map * 2, feat_map, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(feat_map),
+            nn.ReLU(True),
+
+            nn.Conv2d(feat_map, 1, 3, 1, 1, bias=False),
             nn.Tanh()
             # state size. (channels) x 64 x 64
         )
