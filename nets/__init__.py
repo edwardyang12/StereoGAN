9d4117122c842280b577acc426d5f31c3130131f
VictorTao1998 <jit124@ucsd.edu>
2021-08-18 07:45:06 -0700

    a

--- nets/__init__.py
+++ nets/__init__.py
@@ -26,7 +26,7 @@ def find_model_using_name(model_name):
     be instantiated. It has to be a subclass of BaseModel,
     and it is case-insensitive.
     """
-    model_filename = "models." + model_name + "_model"
+    model_filename = "nets." + model_name + "_model"
     modellib = importlib.import_module(model_filename)
     model = None
     target_model_name = model_name.replace('_', '') + 'model'
