6c32fd9cb28da88b99dbe5a84fe6995e787d8d35
VictorTao1998 <jit124@ucsd.edu>
2021-08-18 08:35:14 -0700

    a

--- nets/__init__.py
+++ nets/__init__.py
@@ -59,6 +59,7 @@ def create_model(opt):
         >>> from models import create_model
         >>> model = create_model(opt)
     """
+    opt.model = "cycle_gan"
     model = find_model_using_name(opt.model)
     instance = model(opt)
     print("model [%s] was created" % type(instance).__name__)
