942532ddba9453e0d48166eb2d02a4a1c1a3af22
VictorTao1998 <jit124@ucsd.edu>
2021-08-18 19:55:39 -0700

    a

--- scripts/messy.sh
+++ scripts/messy.sh
@@ -33,7 +33,7 @@ python -m torch.distributed.launch --nproc_per_node=1 /cephfs/jianyu/StereoGAN/g
     --using_ns \
     --ns_size 3 \
     --cmodel gwcnet-c \
-    --logdir "/cephfs/jianyu/eval/cs_train_cgan"  \
+    --logdir "/cephfs/jianyu/eval/cs_train_cgan_test"  \
     --ndisps "48,24" \
     --disp_inter_r "4,1"  \
     --cbatch_size 1 \
