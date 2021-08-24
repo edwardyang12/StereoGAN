985fa75b32c4502303c09d324ca62869b0f8a03f
VictorTao1998 <jit124@ucsd.edu>
2021-08-23 19:42:16 -0700

    a

--- nets/__init__.py
+++ nets/__init__.py
@@ -49,7 +49,7 @@ def get_option_setter(model_name):
     return model_class.modify_commandline_options
 
 
-def create_model(opt, model):
+def create_model(opt, cmodel):
     """Create a model given the option.
 
     This function warps the class CustomDatasetDataLoader.
@@ -62,6 +62,6 @@ def create_model(opt, model):
     #opt.model = "cycle_gan"
     
     model = find_model_using_name(opt.model)
-    instance = model(opt, model)
+    instance = model(opt, cmodel)
     print("model [%s] was created" % type(instance).__name__)
     return instance
